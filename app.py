e8c92d01b1f8f617f5b31c88339224c049f3fd50
pd2 <lukevin89414@gmail.com>
2020-12-25 16:43:50 +0800

    ThirdCommit

--- app.py
+++ app.py
@@ -69,6 +69,9 @@ machine = TocMachine(
     {'trigger': 'advance', 'source': 'body_language', 'dest': 'trivia', 'conditions': 'is_going_to_trivia'},
     {'trigger': 'advance', 'source': 'trivia', 'dest': 'diet', 'conditions': 'is_going_to_diet'},
     {'trigger': 'advance', 'source': 'trivia', 'dest': 'body_language', 'conditions': 'is_going_to_body_language'},
+    {'trigger': 'advance', 'source': 'trivia', 'dest': 'trivia', 'conditions': 'is_going_to_trivia'},
+    {'trigger': 'advance', 'source': 'body_language', 'dest': 'body_language', 'conditions': 'is_going_to_body_language'},
+    {'trigger': 'advance', 'source': 'diet', 'dest': 'diet', 'conditions': 'is_going_to_diet'},
 
     {'trigger': 'advance', 'source': 'diet', 'dest': 'menu', 'conditions': 'is_going_to_menu'},
     {'trigger': 'advance', 'source': 'body_language', 'dest': 'menu', 'conditions': 'is_going_to_menu'},
